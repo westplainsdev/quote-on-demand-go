26a45ac7ca6c3ea2a7bfc883e0b602cfd7fe4398
Chris Berry <chris.berry07@gmail.com>
2019-12-28 21:24:28 -0600

    updated startup message

--- main.go
+++ main.go
@@ -8,33 +8,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func HealthCheck(w http.ResponseWriter, r *http.Request)  {
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "API Is alive and ready")
 }
 
-func getQuote(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "get all endpont")
+func getQuote(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "get all endpoint")
 }
 
-func getQuoteById(w http.ResponseWriter, r *http.Request){
+func getQuoteById(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "get by id endpoint")
 }
 
-func createQuote(w http.ResponseWriter, r *http.Request){
+func createQuote(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "create endpoint")
 }
 
-func updateQuote(w http.ResponseWriter, r *http.Request){
+func updateQuote(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "update endpoint")
 }
 
-func deleteQuote(w http.ResponseWriter, r *http.Request){
+func deleteQuote(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "delete endpoint")
 }
 
+func main() {
 
-func main()  {
-	print("Hello World")
 	router := mux.NewRouter()
 	router.HandleFunc("/api/quote/", getQuote).Methods("GET")
 	router.HandleFunc("/api/quote/{id}", getQuoteById).Methods("GET")
@@ -43,7 +42,8 @@ func main()  {
 	router.HandleFunc("/api/quote/{id}", deleteQuote).Methods("DELETE")
 	router.HandleFunc("/", HealthCheck).Methods("GET")
 
-
-	log.Fatal(http.ListenAndServe(":5000", router))
+	var port = ":5000"
+	print("Listening And Serving on " + port)
+	log.Fatal(http.ListenAndServe(port, router))
 
 }
